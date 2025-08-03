01b2eb2de2dae9fcfd02f020ebaf7d52752695af
v_s <1043064987@qq.com>
2025-08-03 21:18:56 +0800

    publish to github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import glsl from 'vite-plugin-glsl'
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
+    base: '/flight3d/',
     server:
     {
         host: true, // Open to local network and display URL
